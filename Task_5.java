cfd5935393e8a892fd066cf6e63dd6f079ca67fd
DemianenkoVadim <39418363+DemianenkoVadim@users.noreply.github.com>
2018-05-19 10:17:33 +0300

    Add files via upload

---
+++ Task_5.java
@@ -0,0 +1,10 @@
+package com.VDemianenko.Task_5;
+
+public class Task_5 {
+    public static void main (String[]args) {
+        double myEarthWeight = 77.8;
+        double myMoonWeight;
+        myMoonWeight = myEarthWeight * 0.17;
+        System.out.println(myEarthWeight + " кг " + "эквивалентно " + myMoonWeight + " кг на Луне");
+    }
+}
