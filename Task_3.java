68475e44144dc57f0060c920bf498570e86d48ce
DemianenkoVadim <39418363+DemianenkoVadim@users.noreply.github.com>
2018-05-19 10:05:07 +0300

    Add files via upload

---
+++ Task_3.java
@@ -0,0 +1,9 @@
+package com.VDemianenko.Task_3;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        double a = 2.1;
+        int x = (int) (a + 0.5);
+        System.out.println(x);
+    }
+}
