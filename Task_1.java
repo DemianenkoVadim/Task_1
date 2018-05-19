3fc63a5543bd87168734d7304935a27ccfe01641
DemianenkoVadim <39418363+DemianenkoVadim@users.noreply.github.com>
2018-05-19 09:50:44 +0300

    Add files via upload

---
+++ Task_1.java
@@ -0,0 +1,11 @@
+package com.VDemianenko.Task_1;
+
+public class Task_1 {
+    public static void main (String[]args){
+        int q = 34;
+        int w = 15;
+        int x = q / w;
+        int z = q % w;
+        System.out.println(q + " / " + w + " равно " +x +" и "+z+ " в остатке.");
+    }
+}
